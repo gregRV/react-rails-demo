48018d9ad6dab35a627de488eb885cd16eb26c46
Greg RV <gregvarias@gmail.com>
2015-12-02 21:05:45 -0800

    First commit.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  # shorthand for expanded 'respond_to' block- does same thing;
+  # 'tells Rails about all the formats your action knows about'
+  # 'usually, each action in your controller will work with the
+  # same formats. placing respond_to here effects ENTIRE ctrl'
+  respond_to :json
+
+  def index
+    respond_with Comment.all
+  end
+
+  def create
+    respond_with Comment.create(create_params)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author, :comment)
+  end
+end
