9b6a45412363508fd15b284e39fd957004e80a46
Greg RV <gregvarias@gmail.com>
2015-12-03 16:13:30 -0800

    Update readme and create Comment and CommentList components.

--- app/assets/javascripts/components/comments.js.jsx
+++ app/assets/javascripts/components/comments.js.jsx
@@ -1,16 +1,38 @@
-var HelloWorld = React.createClass({
+var Comment= React.createClass({
   render: function(){
     return (
-      <div className='HelloWorld'>
-        Hello, world!
+      <div className='comment'>
+        <h2 className='commentAuthor'>
+          {this.props.author}
+        </h2>
+        {this.props.comment}
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function(){
+    var commentNodes = this.props.comments.map(function(comment, index){
+      return <Comment author={comment.author} comment={comment.comment} key={index} />
+    });
+
+    return (
+      <div className='commentList'>
+        {commentNodes}
       </div>
     );
   }
 });
 
 var ready = function(){
+  var fakeComments = [
+    { author: 'Richard', comment: 'This is a comment' },
+    { author: 'Nils', comment: 'This is another comment' }
+  ];
+
   ReactDOM.render(
-    <HelloWorld />, document.getElementById('comments')
+    <CommentList comments={fakeComments} />, document.getElementById('comments')
   );
 };
 
